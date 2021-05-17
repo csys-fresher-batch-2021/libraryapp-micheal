a530f4193c61a5f4bebb3e56d98f116e6de2265c
Michealwinstonereno V <michealreno28@gmail.com>
2021-05-17 13:28:52 +0530

    All checks have failed error solved

--- src/test/java/in/micheal/test/AdminLoginTest.java
+++ src/test/java/in/micheal/test/AdminLoginTest.java
@@ -16,10 +16,10 @@ public class AdminLoginTest {
 		// Adding Admin 1
 		UserDetails admin1 = new UserDetails();
 		admin1.setUserName("ADMIN 1");
-		admin1.setUserId(9629);
-		admin1.setAdminPassword("9629");
+		admin1.setUserId(4545);
+		admin1.setAdminPassword("4545");
 		AdminRegistration.adminRegistration(admin1);
-		boolean verification = AdminLogin.adminLogin(9629, "9629@libmanagement987");
+		boolean verification = AdminLogin.adminLogin(4545, "4545@libmanagement987");
 		assertTrue(verification);
 
 	}
@@ -30,8 +30,8 @@ public class AdminLoginTest {
 		// Adding Admin 1
 		UserDetails admin2 = new UserDetails();
 		admin2.setUserName("ADMIN 2");
-		admin2.setUserId(8870);
-		admin2.setAdminPassword("8870");
+		admin2.setUserId(4646);
+		admin2.setAdminPassword("4646");
 		AdminRegistration.adminRegistration(admin2);
 		boolean verification = AdminLogin.adminLogin(12345, "9629@libmanagement987");
 		assertFalse(verification);
