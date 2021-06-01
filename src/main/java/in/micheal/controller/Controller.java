4d767cbc81b2ef95f432f13592084e52ed13d02d
Michealwinstonereno V <michealreno28@gmail.com>
2021-06-01 21:02:03 +0530

    Controller for some features added,Some enhancement added

---
+++ src/main/java/in/micheal/controller/Controller.java
@@ -0,0 +1,59 @@
+package in.micheal.controller;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+
+import in.micheal.exception.DbException;
+import in.micheal.model.BookDetail;
+import in.micheal.model.DebtUserDetail;
+import in.micheal.service.AdminService;
+import in.micheal.service.CustomerService;
+
+public class Controller {
+
+	private Controller() {
+		// default constructor
+	}
+
+	/**
+	 * This method is used to control Upload Books servlet
+	 * 
+	 * @param bookName
+	 * @param bookQuantity
+	 * @return
+	 * @throws DbException
+	 */
+	public static String uploadBooksController(String bookName, int bookQuantity) throws DbException {
+		BookDetail obj = new BookDetail();
+		obj.setName(bookName);
+		obj.setQuantity(bookQuantity);
+		return AdminService.uploadBooks(obj);
+	}
+
+	/**
+	 * This method is used to control TakeBook Servlet
+	 * 
+	 * @param bookName
+	 * @param bookQuantity
+	 * @param userId
+	 * @return
+	 * @throws DbException
+	 */
+	public static String takeBookController(String bookName, int bookQuantity, long userId) throws DbException {
+		DebtUserDetail debtUser = new DebtUserDetail();
+		debtUser.setTakenBook(bookName);
+		debtUser.setDebtUserId(userId);
+		debtUser.setTekenBookQuantity(bookQuantity);
+
+		ZoneId defaultZoneId = ZoneId.systemDefault();
+
+		LocalDate localDate = LocalDate.now();
+
+		Date date = Date.from(localDate.atStartOfDay(defaultZoneId).toInstant());
+
+		debtUser.setTakenDate(date);
+
+		return CustomerService.takeBook(debtUser);
+	}
+}
--- src/main/java/in/micheal/servlet/TakeBookAction.java
+++ src/main/java/in/micheal/servlet/TakeBookAction.java
@@ -1,9 +1,6 @@
 package in.micheal.servlet;
 
 import java.io.IOException;
-import java.time.LocalDate;
-import java.time.ZoneId;
-import java.util.Date;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -13,9 +10,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import in.micheal.controller.Controller;
 import in.micheal.exception.DbException;
-import in.micheal.model.DebtUserDetail;
-import in.micheal.service.CustomerService;
 
 /**
  * Servlet implementation class TakeBookAction
@@ -32,20 +28,8 @@ public class TakeBookAction extends HttpServlet {
 		String confirmation;
 		try {
 			int bookQuantity = Integer.parseInt(request.getParameter("bookQuantity"));
-			DebtUserDetail debtUser = new DebtUserDetail();
-			debtUser.setTakenBook(bookName);
-			debtUser.setDebtUserId(loggedInUsername);
-			debtUser.setTekenBookQuantity(bookQuantity);
 
-			ZoneId defaultZoneId = ZoneId.systemDefault();
-
-			LocalDate localDate = LocalDate.now();
-
-			Date date = Date.from(localDate.atStartOfDay(defaultZoneId).toInstant());
-
-			debtUser.setTakenDate(date);
-
-			confirmation = CustomerService.takeBook(debtUser);
+			confirmation = Controller.takeBookController(bookName, bookQuantity, loggedInUsername);
 
 			RequestDispatcher rd = request.getRequestDispatcher("UserView.jsp?msg=" + confirmation);
 			rd.forward(request, response);
